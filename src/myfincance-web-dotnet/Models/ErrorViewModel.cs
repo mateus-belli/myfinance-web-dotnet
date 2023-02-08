718802b7c6518c677f676f5c94ea526b4392a874
Mateus Belli <mateusbelli@hotmail.com>
2023-02-08 20:18:56 -0300

    Initial Commit

---
+++ src/myfincance-web-dotnet/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace myfincance_web_dotnet.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
